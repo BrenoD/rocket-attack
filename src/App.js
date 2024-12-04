94624470fd055c0923d930a65c89e0416c4bf58c
BrenoD <dias2579@gmail.com>
2024-12-04 17:21:34 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
-import { FaGithub } from "react-icons/fa";import { FaDiscord } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaDiscord } from "react-icons/fa";
 import { CiLinkedin } from "react-icons/ci";
 import logo from './assets/imgs/logo-removebg.png';
-import Pgame from "./assets/imgs/rocket_attack_03.png"
+import Pgame from "./assets/imgs/image1.png";
+import game from "./assets/imgs/image.png";
 import './App.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 function App() {
   return (
     <div>
-      <div className="App">
-        <div className='logo'><aside><img src={logo} alt="logo" /></aside></div>
-      <h2><i className='fa fa-download'></i>Download</h2>
-      </div>
-      <div className='content'>
-      <div><aside className='gamephoto'><img src={Pgame} alt='photogame' /></aside></div>
-      <p className='description'>
-        This game is a 2D air rocket game where you control a rocket and dodge missiles
-        while collecting coins to unlock new rockets and upgrade your game. You can play offline and enjoy a thrilling experience.
-      </p>
-      <a href="https://drive.google.com/file/d/128qEvhy_Rf7M9pKqBDCnF8jD-1rl7Y5w/view" className="donw"><p className='dw'><button><i className='fa fa-download'></i>Download</button></p></a>
-      </div>
-      <footer>
-        <div>
-          <p className='propisite'>This site is part of a project to improve React skills.</p>
+      <header className="header">
+        <div className="logo"><img src={logo} alt="logo" /></div>
+        <h2><i className='fa fa-download'></i> Download</h2>
+      </header>
+      <main className='content'>
+        <div className='gamephoto'>
+          <img src={Pgame} alt='Game Screenshot' className="small-photo" />
+          <img src={game} alt='Game Screenshot' className="small-photo" />
         </div>
+        <p className='description'>
+          Experience the thrill of controlling a 2D rocket while dodging missiles and collecting coins to unlock new rockets and upgrades. Play offline and enjoy endless fun.
+        </p>
+        <a href="https://drive.google.com/file/d/128qEvhy_Rf7M9pKqBDCnF8jD-1rl7Y5w/view" className="download-link">
+          <button className='download-button'><i className='fa fa-download'></i> Download</button>
+        </a>
+      </main>
+      <footer className="footer">
+        <p className='propisite'>This site is part of a React skill improvement project.</p>
         <div className="icons">
-          <div className="linkedin"><a href="https://www.linkedin.com/in/breno-dias-11992322a/"><p className="icondisc"><CiLinkedin /></p><p>LinkedIn</p></a></div>
-          <div className="discord"><a href="https://discord.gg/gtdjkBPP"><p className="icondisc"><FaDiscord /></p><p>Discord</p></a></div>
-          <div className="github"><a href="https://github.com/BrenoD"><p className="icondisc"><FaGithub /></p><p>GitHub</p></a></div>
+          <a href="https://www.linkedin.com/in/breno-dias-11992322a/" className="icon linkedin">
+            <CiLinkedin /><span>LinkedIn</span>
+          </a>
+          <a href="https://discord.gg/gtdjkBPP" className="icon discord">
+            <FaDiscord /><span>Discord</span>
+          </a>
+          <a href="https://github.com/BrenoD" className="icon github">
+            <FaGithub /><span>GitHub</span>
+          </a>
         </div>
       </footer>
     </div>
